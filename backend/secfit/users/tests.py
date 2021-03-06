e80a83a3175a1a81fde03868d1b220cf7723be74
KnorpelSenf <shtrog@gmail.com>
2021-03-06 20:03:15 +0100

    Add dummy test

--- backend/secfit/users/tests.py
+++ backend/secfit/users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from users.serializers import UserSerializer
 
 # Create your tests here.
+
+
+class UserSerializerTestCase(TestCase):
+    def setUp(self):
+        self.ser = UserSerializer()
+
+    def test_dummy(self):
+        self.assertTrue(self.ser is not None)
